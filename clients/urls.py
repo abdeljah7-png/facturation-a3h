7170862579df662e5ae5545adea92189776d18f3
Abdel Jah <abdeljah7-png@users.noreply.github.com>
2026-04-19 13:30:46 +0200

    fix ventes client-info url

--- clients/urls.py
+++ clients/urls.py
@@ -3,7 +3,7 @@ from .views import releve_client, releve_client_pdf,client_info
 
 
 urlpatterns = [
-    path('client-info/<int:client_id>/', client_info, name='client_info'),
+    path('ventes/client-info/<int:client_id>/', client_info, name='client_info'),
     path('releve-client/<int:client_id>/', releve_client, name='releve_client'),
     path('releve-client/<int:client_id>/pdf/', releve_client_pdf, name='releve_client_pdf'),
 ]
